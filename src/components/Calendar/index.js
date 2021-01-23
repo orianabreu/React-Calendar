741d0203ca877b99bba9846872bfb192ebd8004d
Oriana Abreu <oriabreu.v@gmail.com>
2021-01-23 13:18:07 +0100

    change calendar to react-big-calendar

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,16 +1,40 @@
-import React, { useState } from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import React from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+require('moment/locale/es.js');
+
+const localizer = momentLocalizer(moment);
 
 export default function MyCalendar() {
-  const [value, onChange] = useState(new Date());
 
-  return (
-    <div>
-      <Calendar
-        onChange={(value, event) => alert('New date is: ', value)}
-        value={value}
-      />
-    </div>
-  );
+    const events = [
+        {
+          start: moment().toDate(),
+          end: moment()
+            .add(1, "days")
+            .toDate(),
+          title: "Some title"
+        }
+      ]
+
+    return (
+        <div className="App">
+            <Calendar
+            localizer={localizer}
+            defaultDate={new Date()}
+            defaultView="month"
+            events={events}
+            style={{ height: "80vh", width: '80vw' }}
+            messages={{
+                next: "sig",
+                previous: "ant",
+                today: "Hoy",
+                month: "Mes",
+                week: "Semana",
+                day: "Día"
+              }}
+            />
+      </div>
+    );
 }
